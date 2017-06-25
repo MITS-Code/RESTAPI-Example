9238d413e6cbfc6974913a08fb1a36bd89004cf5
Grant Visser <grantrvisser@gmail.com>
2017-06-26 00:02:10 +0200

    Initial structure and Code.

---
+++ RESTAPIExample/RESTAPIExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTAPIExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Out.WriteLine("This is still very BETA");
+           Console.ReadKey();
+        }
+    }
+}
